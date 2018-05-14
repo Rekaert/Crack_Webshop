99eb000d232c510964fff01267a63e1f898a07fb
Ertsey Réka <rekaert@gmail.com>
2018-05-14 10:47:03 +0200

    Feat: add components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+const routes: Routes = [
+  { path: '', component: NavbarComponent, pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'orders', component: OrdersComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UsersComponent,
+    ProductsComponent,
+    OrdersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
