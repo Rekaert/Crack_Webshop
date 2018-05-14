786af48a42465167a153264e7d701d2b9d38d16e
Andrea Tóth-Gayer <gayerandi@yahoo.com>
2018-05-14 13:43:43 +0200

    user component

---
+++ frontend/src/app/users/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    username: string;
+    email: string;
+    szmlcim: string;
+    szallcim: string;
+    perm: number;
+    password: string;
+    id: string;
+}
---
+++ frontend/src/app/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+
+@Injectable()
+export class UsersService {
+  private url = 'http://localhost:8080/user/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.url);
+  }
+
+  delete(Userid): Observable<void> {
+    console.log('Deleting ID: ' + Userid);
+    return this.httpClient.delete<void>(this.url + Userid);
+  }
+
+  save(user: User): Observable<User> {
+    if (user.id) {
+      return this.httpClient.put<User>(this.url + user.id, user);
+    } else {
+      return this.httpClient.post<User>(this.url, user);
+    }
+  }
+
+}
