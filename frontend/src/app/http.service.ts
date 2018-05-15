3227c9917ab73eff51b167363a5baacf9673c784
Kovats Oliver <koviko@gmail.com>
2018-05-15 12:40:15 +0200

    dashnoard teljesen működőképes

--- frontend/src/app/http.service.ts
+++ frontend/src/app/http.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { User } from './users/user';
-
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
+import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class HttpLocalService {
   private url = 'http://localhost:8080';
@@ -13,7 +14,15 @@ export class HttpLocalService {
 
   getUsers() {
     this.httpClient.get(this.url + '/user/')
-      .subscribe((data) => this.users = data);
+      .subscribe((data) => {
+        this.users = data;
+
+      });
+  }
+  getCost(): Promise<any> {
+    return this.httpClient
+      .get(this.url + '/order/all')
+      .toPromise()
   }
   getOrders() {
     this.httpClient.get(this.url + '/order/all')
