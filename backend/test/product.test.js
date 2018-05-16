f12e7708f1709fc95aefe3685b54ab56f7155d2f
Szegedi Daniel <szegedi.dani22@gmail.com>
2018-05-16 19:49:33 +0200

    Adding tests to product.controller functions

--- backend/test/product.test.js
+++ backend/test/product.test.js
@@ -50,45 +50,113 @@ describe('Product', () => {
         });
     });
     it('request should have parameter id', (done) => {
-      post.save((err, post) => {
-        chai.request(baseUrl)
-          .get(`/${productId}`)
-          .send(book)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('name');
-            res.body.should.have.property('url');
-            res.body.should.have.property('image');
-            res.body.should.have.property('manufacturer');
-            res.body.should.have.property('_id').eql(post._id);
-            done();
-          });
-      });
+      chai.request(baseUrl)
+        .get(`/${productId}`)
+        .end((err, res) => {
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('url');
+          res.body.should.have.property('image');
+          res.body.should.have.property('manufacturer');
+          res.body.should.have.property('_id').eql(productId);
+          done();
+        });
+    });
+  });
+  describe('create()', () => {
+    it('response statusCode equal to 200', (done) => {
+      chai.request(baseUrl)
+        .get('/')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('response should be an object', (done) => {
+      chai.request(baseUrl)
+        .get('/')
+        .end((err, res) => {
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+    it('request should have parameter id', (done) => {
+      chai.request(baseUrl)
+        .get(`/${productId}`)
+        .end((err, res) => {
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('url');
+          res.body.should.have.property('image');
+          res.body.should.have.property('manufacturer');
+          res.body.should.have.property('_id').eql(productId);
+          done();
+        });
+    });
+  });
+  describe('update()', () => {
+    it('response statusCode equal to 200', (done) => {
+      chai.request(baseUrl)
+        .get(`/${productId}`)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('response should be an object', (done) => {
+      chai.request(baseUrl)
+        .get(`/${productId}`)
+        .end((err, res) => {
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+    it('request should have parameter id', (done) => {
+      chai.request(baseUrl)
+        .get(`/${productId}`)
+        .end((err, res) => {
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('url');
+          res.body.should.have.property('image');
+          res.body.should.have.property('manufacturer');
+          res.body.should.have.property('_id').eql(productId);
+          done();
+        });
+    });
+  });
+  describe('delete()', () => {
+    it('response statusCode equal to 200', (done) => {
+      chai.request(baseUrl)
+        .get(`/${productId}`)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('response should be an object', (done) => {
+      chai.request(baseUrl)
+        .get(`/${productId}`)
+        .end((err, res) => {
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+    it('request should have parameter id', (done) => {
+      chai.request(baseUrl)
+        .get(`/${productId}`)
+        .end((err, res) => {
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('url');
+          res.body.should.have.property('image');
+          res.body.should.have.property('manufacturer');
+          res.body.should.have.property('_id').eql(productId);
+          done();
+        });
     });
   });
 });
-/**
- * let post = new Product({
-        "_id": "5afadcfc28fcf422f08c2857",
-        "name": "Tron Legacy",
-        "url": "tron-legace",
-        "image": "tron-legace.jpg",
-        "manufacturer": "Lego",
-        "cost": 50000,
-      });
-      post.save((err, post) => {
-        chai.request(baseUrl)
-          .get('/' + post.id)
-          .send(book)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('title');
-            res.body.should.have.property('content');
-            res.body.should.have.property('userid');
-            res.body.should.have.property('_id').eql(post.id);
-            done();
-          });
-      });
- */
