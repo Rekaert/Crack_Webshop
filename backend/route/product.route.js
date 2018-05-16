8d4d99183f73587608d4dd2b84e754ed977ea71d
Szegedi Daniel <szegedi.dani22@gmail.com>
2018-05-16 13:33:43 +0200

    Documentation done

--- backend/route/product.route.js
+++ backend/route/product.route.js
@@ -3,35 +3,31 @@ const ProductController = require('../controller/product.controller');
 
 /**
  * @module Product-router
+ * routing middleware for products
  */
 /**
- * Routing middleware for products
  * @method list - get request to '/'
  * @desc Get all products
  */
 productRouter.get('/', ProductController.list);
 /**
- * Routing middleware for products
  * @method find - get request to '/'
  * @desc Find a product
  * @param {Number} id - The id of the product
  */
 productRouter.get('/:id', ProductController.find);
 /**
- * Routing middleware for products
  * @method create - post request to '/'
  * @desc Create a new product
  */
 productRouter.post('/', ProductController.create);
 /**
- * Routing middleware for products
  * @method update - put request to '/'
  * @desc Update a product
  * @param {Number} id - The id of the product
  */
 productRouter.put('/:id', ProductController.update);
 /**
- * Routing middleware for products
  * @method remove - delete request to '/'
  * @desc Delete a product
  * @param {Number} id -The id of the product
--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -4,46 +4,40 @@ const UserController = require('../controller/user.controller');
 
 /**
  * @module User-router
+ * routing middleware for users
  */
 /**
- * Routing middleware for Users
  * @method profile - get request to '/profile'
  * @desc - Get the logged in user
  */
 userRouter.get('/profile', UserController.profile);
 /**
- * Routing middleware for Users
  * @method all - get request to '/'
  * @desc - Get all users
  */
 userRouter.get('/', UserController.all);
 /**
- * Routing middleware for Users
  * @method logout - get request to '/logout'
  */
 userRouter.get('/logout', UserController.logout);
 /**
- * Routing middleware for Users
  * @method register - post request to '/register'
  * @desc Register a new user
  */
 userRouter.post('/register', UserController.register);
 /**
- * Routing middleware for Users
  * @method update - post request to '/update'
  * @desc Update a user
  * @param {Number} id - The id of the user
  */
 userRouter.post('/update/:id', UserController.update);
 /**
- * Routing middleware for Users
  * @method delete - delete request to '/delete'
  * @desc Delete a user
  * @param {Number} id - The id of the user
  */
 userRouter.post('/delete/:id', UserController.delete);
 /**
- * Routing middleware for Users
  * @method login - post request to '/login'
  * @desc Authenticate and log in user
  */
