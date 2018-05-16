0c8370390bc75bb5134496f1c89c51768bf44a9c
Szegedi Daniel <szegedi.dani22@gmail.com>
2018-05-16 13:12:22 +0200

    jsdoc comments for routers

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -3,21 +3,50 @@ const userRouter = require('express').Router();
 const UserController = require('../controller/user.controller');
 
 /**
- * Routing middleware
- * @method profile - Get user
- * @method all - List all users
- * @method register - Register a new user
- * @method update - Update a users properties
- * @method delete - Delete a user
- * @method login - Log a user in
- * @method logout - Log a user out
+ * @module User-router
+ */
+/**
+ * Routing middleware for Users
+ * @method profile - get request to '/profile'
+ * @desc - Get the logged in user
  */
 userRouter.get('/profile', UserController.profile);
+/**
+ * Routing middleware for Users
+ * @method all - get request to '/'
+ * @desc - Get all users
+ */
 userRouter.get('/', UserController.all);
-userRouter.post('/register', UserController.register);
-userRouter.post('/update/:id', UserController.update);
-userRouter.post('/delete/:id', UserController.delete);
-userRouter.post('/login', passport.authenticate('local'), UserController.login);
+/**
+ * Routing middleware for Users
+ * @method logout - get request to '/logout'
+ */
 userRouter.get('/logout', UserController.logout);
+/**
+ * Routing middleware for Users
+ * @method register - post request to '/register'
+ * @desc Register a new user
+ */
+userRouter.post('/register', UserController.register);
+/**
+ * Routing middleware for Users
+ * @method update - post request to '/update'
+ * @desc Update a user
+ * @param {Number} id - The id of the user
+ */
+userRouter.post('/update/:id', UserController.update);
+/**
+ * Routing middleware for Users
+ * @method delete - delete request to '/delete'
+ * @desc Delete a user
+ * @param {Number} id - The id of the user
+ */
+userRouter.post('/delete/:id', UserController.delete);
+/**
+ * Routing middleware for Users
+ * @method login - post request to '/login'
+ * @desc Authenticate and log in user
+ */
+userRouter.post('/login', passport.authenticate('local'), UserController.login);
 
 module.exports = userRouter;
