2b9fc17831c88962a19e19857c19a47fe1735314
Andrea Tóth-Gayer <gayerandi@yahoo.com>
2018-05-17 10:48:51 +0200

    Fix: user test

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -6,77 +6,41 @@ const userController = require('../controller/user.controller');
 const expect = require('chai').expect;
 const chaiHttp = require('chai-http');
 
-const baseUrl = 'http://localhost:8080/user/';
+const baseUrl = 'http://localhost:8080/user';
 const userId = '5afc8d128ec0691788342495';
 chai.use(chaiHttp);
 
 const theAccount = {
-
   username: 'testAdmin@gmail.com',
-
   password: '1234',
-
 };
 
 let cookie;
 
-/**
+// test - user controller
 
-* useradmin.controller összevont unit teszt
+// test - update a specific user - register and logged in
 
-* @todo debug test of editUser, removeUser
-
-*/
-
-describe('user.controller functions', () => {
-  // test - get user profile
-  describe('profile()', () => {
-    it('response statusCode equal to 200 and object in res', (done) => {
-      chai.request(baseUrl)
-
-        .get('/profile')
-
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-
-          expect(res).to.be.an('object');
-
-          done();
-        });
-    });
+describe('User', () => {
+  before((done) => {
+    chai.request('http://localhost:8080/user')
+      .post('/login')
+      .send(theAccount)
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        cookie = res.headers['set-cookie'].pop().split(';')[0];
+        done();
+      });
   });
-
-  // test - get all users
-
-  describe('all()', () => {
-    it('response statusCode equal to 200 and object in res', (done) => {
-      chai.request(baseUrl)
-
-        .get('/')
-
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-
-          expect(res).to.be.an('array');
-
-          done();
-        });
-    });
-  });
-
-  // test - register a new user
-
   describe('register()', () => {
     it('response statusCode equal to 200 and object in res', (done) => {
       chai.request(baseUrl)
-
         .post('/register')
-
         .end((err, res) => {
           expect(res).to.have.status(200);
-
           expect(res).to.be.an('object');
-
           expect(res).to.have.property('username');
           expect(res).to.have.property('email');
           expect(res).to.have.property('szmlcim');
@@ -89,32 +53,36 @@ describe('user.controller functions', () => {
     });
   });
 
-  // test - update a specific user - logged in
-
-  describe('update()', () => {
-    // Login során kapunk egy sütit a http headerbe, ezt lementjük a süti változóba
-
-    // Ez azért kell mert minden kérésnél, amihez szükséges a belépett user,
-
-    // el kell küldeni a kapott sütit is. Hiszen ez azonosítja a usert
-
-    // Itt nincs böngésző ami lementse, így manuálisan kell
-
-    before((done) => {
-      chai.request('http://localhost:8080/user')
-
-        .post('/login')
-
-        .send(theAccount)
-
+  // test - get user profile
+  describe('profile()', () => {
+    it('response statusCode equal to 200 and object in res', (done) => {
+      chai.request(baseUrl)
+        .get('/profile')
         .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          cookie = res.headers['set-cookie'].pop().split(';')[0];
+          expect(res).to.have.status(200);
+          expect(res).to.be.an('object');
           done();
         });
     });
+  });
+
+  // test - get all users
+
+  describe('all()', () => {
+    it('response statusCode equal to 200 and object in res', (done) => {
+      chai.request(baseUrl)
+        .get('/')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.an('array');
+          done();
+        });
+    });
+  });
+
+
+  describe('update()', () => {
+
 
     it('response statusCode equal to 200', (done) => {
       chai.request(baseUrl)
@@ -150,6 +118,7 @@ describe('user.controller functions', () => {
         });
     });
   });
+
   // test - delete user
   describe('delete()', () => {
     it('response statusCode equal to 200', (done) => {
