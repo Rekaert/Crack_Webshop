493e85d3e4e5a6412d96a02966efa53fc650a98c
Kovats Oliver <koviko@gmail.com>
2018-05-18 16:07:48 +0200

    főoldal alap

--- frontend/src/app/fooldal/fooldal.component.ts
+++ frontend/src/app/fooldal/fooldal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-fooldal',
@@ -6,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fooldal.component.css']
 })
 export class FooldalComponent implements OnInit {
+  products: any = [];
+  item: any = 3;
+  constructor(public http: Http) {
+    this.getAll();
+  }
+  getAll() {
+    this.http.get('http://localhost:8080/product').subscribe(
+      data => {
+        data = JSON.parse(data['_body']);
+        this.fillRow(data, this.item);
+      });
+  }
+  fillRow(data, num) {
+    for (let i = 0; i < data.length / num; i++) {
+      let arr = [];
+      for (let j = 0; j < num; j++) {
 
-  constructor() { }
+        arr.push(data[i * num + j]);
 
+      }
+      this.products.push(arr);
+    }
+    console.log(this.products);
+  }
+  changeRow(num) {
+    this.products = [];
+    this.http.get('http://localhost:8080/product').subscribe(
+      data => {
+        data = JSON.parse(data['_body']);
+        this.fillRow(data, num);
+      });
+  }
   ngOnInit() {
   }
 
