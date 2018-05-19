e36d58c77c1841c07332874ecb7eabce3078c087
Kovats Oliver <koviko@gmail.com>
2018-05-19 13:29:35 +0200

    edited routes

--- frontend/src/app/selectedproduct/selectedproduct.component.ts
+++ frontend/src/app/selectedproduct/selectedproduct.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-selectedproduct',
   templateUrl: './selectedproduct.component.html',
   styleUrls: ['./selectedproduct.component.css']
 })
 export class SelectedproductComponent implements OnInit {
+  public id: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    console.log(this.id);
   }
 
 }
